fcffb19f174f112df52cd675c6ebec60738fba3c
jkm01 <jacqueskatsuva@gmail.com>
2021-12-06 20:39:47 +0300

    Ternaire Js with emoji

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import '../styles/App.css';
 import Banner from './Banner';
 import Cart from './Cart';
+import ShoppingList from './ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <ShoppingList />
       <Cart />
     </div>
   );
